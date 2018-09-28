c0b2a085a4df313ba662c7fb37bedd1a447780f2
Sourabh S. Rawat <dragneelfps@gmail.com>
2018-09-28 22:38:37 +0530

    Initial commit

---
+++ app/src/main/java/com/nooblabs/srawa/quizapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nooblabs.srawa.quizapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quiz_question_content)
+    }
+}
